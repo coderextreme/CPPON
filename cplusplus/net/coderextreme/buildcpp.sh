0a2c5d502389cd8e2af111c5e38fe3b17276d867
John Carlson <yottzumm@gmail.com>
2024-04-04 08:05:51 -0500

    Getting ANTLR running

--- cplusplus/net/coderextreme/buildcpp.sh
+++ cplusplus/net/coderextreme/buildcpp.sh
@@ -3,10 +3,5 @@ mkdir -p cpp
 pushd cpp
 java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." org.antlr.v4.Tool -Dlanguage=Cpp -visitor ../CPPONGrammar.g4
 export PATH=${PATH}:/c/msys64/usr/bin
-C:/msys64/usr/bin/g++.exe -I/c/msys64/usr/include -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/x86_64-pc-msys -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/tr1/ -I/c/Users/john/Downloads/antlr4-cpp-runtime-4.13.1-source/runtime/src/ -o CPP.exe *.cpp -LC/:msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0 -lstdc++
-for i in ../data/*.h
-do
-	echo $i
-	# CPP.exe "$i"
-done
+C:/msys64/usr/bin/g++.exe -I. -I/c/Users/john/Downloads/antlr4-cpp-runtime-4.13.1-source/runtime/src/ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++ -I/c/msys64/usr/include -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/x86_64-pc-msys -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/tr1/ *.cpp -LC/:msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0 -lstdc++ -o CPP.exe
 popd
--- cplusplus/net/coderextreme/buildjava.sh
+++ cplusplus/net/coderextreme/buildjava.sh
@@ -2,10 +2,11 @@
 mkdir -p java
 pushd java
 # antlr4.exe -vistor ../CPPONGrammar.g4
-java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." org.antlr.v4.Tool -Dlanguage=Cpp -visitor ../CPPONGrammar.g4
+java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." org.antlr.v4.Tool -Dlanguage=Java -visitor ../CPPONGrammar.g4
 javac -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." *.java
 for i in ../data/*.h
 do
 	echo $i
 	java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." CPP $i
 done
+popd
