3e14e9625caccfc610ddd8e38ec47351711c356a
John Carlson <yottzumm@gmail.com>
2024-04-04 10:12:08 -0500

    Removed count when creating array.  I don't want any memmory issues

--- cplusplus/net/coderextreme/buildparser.sh
+++ cplusplus/net/coderextreme/buildparser.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-antlr4.exe CPPONGrammar.g4
-for i in data/[bdeghil-oqs-zBDEGHIL-OQS-Z]*.h
+# antlr4.exe CPPONGrammar.g4
+for i in data/*.h
 do
 	echo $i
 	cat "$i" | antlr4-parse.exe CPPONGrammar.g4 x3d
