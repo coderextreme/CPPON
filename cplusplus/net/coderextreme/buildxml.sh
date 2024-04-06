ca3d37d8464d10e92995d5918d3631ffcbcf59c0
John Carlson <yottzumm@gmail.com>
2024-04-06 05:26:10 -0500

    don't process afxtempl.h

--- cplusplus/net/coderextreme/buildxml.sh
+++ cplusplus/net/coderextreme/buildxml.sh
@@ -3,8 +3,11 @@
 
 for i in data/*.h
 do
-	echo $i
-	X3D=../../../data/`basename $i .h`.x3d
-	echo $i | sed 's/^/..\//' | xargs bash buildjava.sh > "$X3D"
-	java -cp ~/pythonSAI/X3DJSAIL.4.0.full.jar org.web3d.x3d.jsail.CommandLine -validate $X3D
+	if [ "$i" != "data/afxtempl.h" ]
+	then
+		echo $i
+		X3D=../../../data/`basename $i .h`.x3d
+		echo $i | sed 's/^/..\//' | xargs bash buildjava.sh > "$X3D"
+		java -cp ~/pythonSAI/X3DJSAIL.4.0.full.jar org.web3d.x3d.jsail.CommandLine -validate $X3D
+	fi
 done
