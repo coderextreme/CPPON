3e656f9dc1cc428541e0c92261b157154abf568d
John W Carlson <yottzumm@gmail.com>
2025-08-22 09:21:17 -0500

    changed cpp and js scripts, deleted big files

--- cplusplus/net/coderextreme/buildcpp.sh
+++ cplusplus/net/coderextreme/buildcpp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p cpp
 pushd cpp
-java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." org.antlr.v4.Tool -Dlanguage=Cpp -visitor ../CPPONGrammar.g4
+java -Xss1g -Xmx8g -cp "C:/Users/jcarl/Downloads/antlr-4.13.2-complete.jar;." org.antlr.v4.Tool -Dlanguage=Cpp -visitor ../CPPONGrammar.g4
 export PATH=${PATH}:/c/msys64/usr/bin
 C:/msys64/usr/bin/g++.exe -I. -I/c/Users/john/Downloads/antlr4-cpp-runtime-4.13.1-source/runtime/src/ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++ -I/c/msys64/usr/include -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/ -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/x86_64-pc-msys -I/c/msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0/include/c++/tr1/ *.cpp -LC/:msys64/usr/lib/gcc/x86_64-pc-msys/11.3.0 -lstdc++ -o CPP.exe
 popd
--- cplusplus/net/coderextreme/buildjs.sh
+++ cplusplus/net/coderextreme/buildjs.sh
@@ -4,7 +4,7 @@ pushd js
 #npm install -g antlr4js-cli
 npm install --save antlr4
 #antlr4js -visitor ../CPPONGrammar.g4
-java -Xss1g -Xmx8g -cp "C:/Users/john/Downloads/antlr-4.13.1-complete.jar;." org.antlr.v4.Tool -Dlanguage=JavaScript -visitor ../CPPONGrammar.g4
+java -Xss1g -Xmx8g -cp "C:/Users/jcarl/Downloads/antlr-4.13.2-complete.jar;." org.antlr.v4.Tool -Dlanguage=JavaScript -visitor ../CPPONGrammar.g4
 for i in ../data/*.h
 do
 	echo $i
