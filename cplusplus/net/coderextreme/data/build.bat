4631a9f2fd76f960ff54780d7764fe79db840229
John Carlson <yottzumm@gmail.com>
2026-04-01 04:17:26 -0500

    Removed CPP and H code

--- cplusplus/net/coderextreme/data/build.bat
+++ cplusplus/net/coderextreme/data/build.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
+"C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsamd64_x86.bat"
 FOR %i IN (*.cpp) DO cl.exe /c %i /EHsc
