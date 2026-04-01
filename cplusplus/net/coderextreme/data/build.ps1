25813cd0399c207cf103cd8f716a84806ab42fc2
John Carlson <yottzumm@gmail.com>
2026-04-01 04:20:36 -0500

    Added a build

---
+++ cplusplus/net/coderextreme/data/build.ps1
@@ -0,0 +1,7 @@
+$cppFiles = Get-ChildItem -Filter "*.cpp"
+
+foreach ($file in $cppFiles) {
+    Write-Host "Processing file: $($file.Name)"
+    Write-Host "Full path: $($file.FullName)"
+    cl.exe /c $file.Name /I . /EHsc
+}
